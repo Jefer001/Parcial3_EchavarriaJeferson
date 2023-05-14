c2d16c2edc5e32a7c889fe6abd9fce3105df02f0
jefferson echavarria <jfrson__@live.com>
2023-05-14 00:28:39 -0500

    CRUD VehicleController.

--- WashingCar/Controllers/VehiclesController.cs
+++ WashingCar/Controllers/VehiclesController.cs
@@ -1,49 +1,49 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.EntityFrameworkCore;
-using System.Diagnostics.Metrics;
 using WashingCar.DAL;
 using WashingCar.DAL.Entities;
 
 namespace WashingCar.Controllers
 {
-    public class ServicesController : Controller
+    public class VehiclesController : Controller
     {
         #region Constants
         private readonly DataBaseContext _context;
         #endregion
 
         #region Builder
-        public ServicesController(DataBaseContext context)
+        public VehiclesController(DataBaseContext context)
         {
             _context = context;
         }
         #endregion
 
-        #region Service actions
-        // GET: Services
+        #region Vehicle actions
+        // GET: Vehicles
         public async Task<IActionResult> Index()
         {
             return _context.Services != null ?
-                        View(await _context.Services
-                        .Include(v => v.Vehicles)
+                        View(await _context.Vehicles
+                        .Include(v => v.VehicleDetails)
                         .ToListAsync()) :
                         Problem("Entity set 'DataBaseContext.Services'  is null.");
         }
 
-        // GET: Services/Details/5
+        // GET: Vehicles/Details/5
         public async Task<IActionResult> Details(Guid? id)
         {
-            if (id == null || _context.Services == null) return NotFound();
+            if (id == null || _context.Vehicles == null) return NotFound();
 
-            var service = await _context.Services
-                .Include(v => v.Vehicles)
+            var vehicle = await _context.Vehicles          
                 .FirstOrDefaultAsync(m => m.Id.Equals(id));
-            if (service == null) return NotFound();
 
-            return View(service);;
+            if (vehicle == null) return NotFound();
+
+            return View(vehicle); ;
         }
 
-        // GET: Services/Create
+        // GET: Vehicles/Create
         public IActionResult Create()
         {
             return View();
@@ -51,14 +51,14 @@ namespace WashingCar.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(Service service)
+        public async Task<IActionResult> Create(Vehicle vehicle)
         {
             if (ModelState.IsValid)
             {
                 try
                 {
-                    service.Id = Guid.NewGuid();
-                    _context.Add(service);
+                    vehicle.Id = Guid.NewGuid();
+                    _context.Add(vehicle);
                     await _context.SaveChangesAsync();
                     return RedirectToAction(nameof(Index));
 
@@ -66,8 +66,7 @@ namespace WashingCar.Controllers
                 catch (DbUpdateException dbUpdateException)
                 {
                     if (dbUpdateException.InnerException.Message.Contains("duplicate"))
-                        ViewBag.ErrorMessage("Ya existe un servicio con el mismo nombre.");
-                    //ModelState.AddModelError(string.Empty, "Ya existe un país con el mismo nombre.");
+                        ModelState.AddModelError(string.Empty, "Ya existe un vehiculo con el mismo nombre.");
                     else
                         ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
                 }
@@ -76,39 +75,40 @@ namespace WashingCar.Controllers
                     ModelState.AddModelError(string.Empty, ex.Message);
                 }
             }
-            return View(service);
+            return View(vehicle);
         }
 
-        // GET: Services/Edit/5
+        // GET: Vehicles/Edit/5
         public async Task<IActionResult> Edit(Guid? id)
         {
-            if (id == null || _context.Services == null) return NotFound();
+            if (id == null || _context.Vehicles == null) return NotFound();
 
-            var service = await _context.Services.FindAsync(id);
-
-            if (service == null) return NotFound();
-
-            return View(service);
+            var vehicle = await _context.Vehicles.FindAsync(id);
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+            return View(vehicle);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(Guid id, Service service)
+        public async Task<IActionResult> Edit(Guid id, Vehicle vehicle)
         {
-            if (id != service.Id) return NotFound();
+            if (id != vehicle.Id) return NotFound();
 
             if (ModelState.IsValid)
             {
                 try
                 {
-                    _context.Update(service);
+                    _context.Update(vehicle);
                     await _context.SaveChangesAsync();
                     return RedirectToAction(nameof(Index));
                 }
                 catch (DbUpdateException dbUpdateException)
                 {
                     if (dbUpdateException.InnerException.Message.Contains("duplicate"))
-                        ModelState.AddModelError(string.Empty, "Ya existe un servicio con el mismo nombre.");
+                        ModelState.AddModelError(string.Empty, "Ya existe un vehiculo con el mismo nombre.");
                     else
                         ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
                 }
@@ -117,36 +117,41 @@ namespace WashingCar.Controllers
                     ModelState.AddModelError(string.Empty, exception.Message);
                 }
             }
-            return View(service);
+            return View(vehicle);
         }
 
-        // GET: Services/Delete/5
+        // GET: Vehicles/Delete/5
         public async Task<IActionResult> Delete(Guid? id)
         {
-            if (id == null || _context.Services == null) return NotFound();
+            if (id == null || _context.Vehicles == null) return NotFound();
 
-            var service = await _context.Services
+            var vehicle = await _context.Vehicles
                 .FirstOrDefaultAsync(m => m.Id.Equals(id));
 
-            if (service == null) return NotFound();
+            if (vehicle == null) return NotFound();
 
-            return View(service);
+            return View(vehicle);
         }
 
-        // POST: Services/Delete/5
+        // POST: Vehicles/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(Guid id)
         {
-            if (_context.Services == null) return Problem("Entity set 'DataBaseContext.Services'  is null.");
+            if (_context.Vehicles == null) return Problem("Entity set 'DataBaseContext.Vehicle'  is null.");
 
-            var service = await _context.Services.FindAsync(id);
+            var service = await _context.Vehicles.FindAsync(id);
 
-            if (service != null) _context.Services.Remove(service);
+            if (service != null) _context.Vehicles.Remove(service);
 
             await _context.SaveChangesAsync();
 
             return RedirectToAction(nameof(Index));
+        }
+
+        private bool VehicleExists(Guid id)
+        {
+            return _context.Vehicles.Any(e => e.Id == id);
         }
         #endregion
     }
